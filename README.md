f43700f7c4512246c69ffeeca59c7319f790cf95
Sanjog Sharma <sanjogsharma@gmail.com>
2017-01-16 03:11:05 +0000

    validation added

---
+++ README.md
@@ -0,0 +1,4 @@
+*Free Code Camp Voting App Project*
+
+https://www.freecodecamp.com/challenges/build-a-voting-app
+
--- app/routes/index.js
+++ app/routes/index.js
@@ -198,7 +198,12 @@ module.exports = function(app, passport) {
 		});
 
 
-	app.route('/logout')
+	app.route('/login')
+ 		.get(function(req, res) {
+ 			res.sendFile(path + '/public/login.html');
+ 		});
+		
+		app.route('/logout')
 		.get(function(req, res) {
 			req.logout();
 			res.redirect('/');
